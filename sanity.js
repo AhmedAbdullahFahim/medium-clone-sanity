418400c63b2cca7afbcf32387d316242b5fc916b
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-01-01 01:27:48 +0200

    edited import path @sanity/image-url

--- sanity.js
+++ sanity.js
@@ -1,6 +1,6 @@
 import { createClient } from 'next-sanity'
 
-import createImageUrlBuilder from './@sanity/image-url'
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
