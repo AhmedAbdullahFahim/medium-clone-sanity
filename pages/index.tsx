69c234f5c94817b85d1656c81fd94a8276b5edef
ahmedabdullahfahim <ahmed.abdallah5022@gmail.com>
2023-02-28 22:40:41 +0200

    added logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({ posts }: Props) {
     <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/logo.png' />
         <meta charSet='utf-8' />
       </Head>
       <Banner />
